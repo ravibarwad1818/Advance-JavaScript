6447d96333eda927a5d24c6b0670475d3e70034e
ravibarwad1818 <108672879+ravibarwad1818@users.noreply.github.com>
2022-08-14 21:11:34 +0530

    Add files via upload

--- problem2.js
+++ problem2.js
@@ -1,82 +1,24 @@
-const projectName = 'random-quote-machine';
-let quotesData;
-
-var colors = [
-  '#16a085',
-  '#27ae60',
-  '#2c3e50',
-  '#f39c12',
-  '#e74c3c',
-  '#9b59b6',
-  '#FB6964',
-  '#342224',
-  '#472E32',
-  '#BDBB99',
-  '#77B1A9',
-  '#73A857'
-];
-var currentQuote = '',
-  currentAuthor = '';
-
-function getQuotes() {
-  return $.ajax({
-    headers: {
-      Accept: 'application/json'
-    },
-    url:
-      'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
-    success: function (jsonQuotes) {
-      if (typeof jsonQuotes === 'string') {
-        quotesData = JSON.parse(jsonQuotes);
-        console.log('quotesData');
-        console.log(quotesData);
-      }
-    }
-  });
-}
-
-function getRandomQuote() {
-  return quotesData.quotes[
-    Math.floor(Math.random() * quotesData.quotes.length)
-  ];
-}
-
-function getQuote() {
-  let randomQuote = getRandomQuote();
-
-  currentQuote = randomQuote.quote;
-  currentAuthor = randomQuote.author;
-
-  $('.quote-text').animate({ opacity: 0 }, 500, function () {
-    $(this).animate({ opacity: 1 }, 500);
-    $('#text').text(randomQuote.quote);
-  });
-
-  $('.quote-author').animate({ opacity: 0 }, 500, function () {
-    $(this).animate({ opacity: 1 }, 500);
-    $('#author').html(randomQuote.author);
-  });
-
-  var color = Math.floor(Math.random() * colors.length);
-  $('html body').animate(
-    {
-      backgroundColor: colors[color],
-      color: colors[color]
-    },
-    1000
-  );
-  $('.button').animate(
-    {
-      backgroundColor: colors[color]
-    },
-    1000
-  );
-}
-
-$(document).ready(function () {
-  getQuotes().then(() => {
-    getQuote();
-  });
-
-  $('#new-quote').on('click', getQuote);
+const sliders = document.querySelectorAll("input[type='range']");
+sliders.forEach(function(slider){
+    slider.addEventListener("input",calculateTip);
 });
+
+const billInput = document.getElementById("bill");
+billInput.addEventListener("change",calculateTip);
+
+
+function calculateTip(){
+    let bill = parseFloat(billInput.value);
+    let tipPercent = document.getElementById("tip").value;
+
+    billInput.value = bill.toFixed(2);
+
+    let totalTip = parseFloat((bill * (tipPercent/100)).toFixed(2));
+    let total = parseFloat((bill + totalTip).toFixed(2));
+
+    document.getElementById("tip-amount").textContent = `\$ ${totalTip}`;
+    document.getElementById("total-amount").textContent = `\$ ${total}`;
+    
+    document.getElementById("tip-percent").textContent = `${tipPercent}%`;
+}
+calculateTip();
